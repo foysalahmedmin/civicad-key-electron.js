24ce850f484bf3e044f2bb83cbdc58f42020fc41
Foysal Ahmed <121689537+foysalahmedmin@users.noreply.github.com>
2025-05-20 12:26:31 +0600

    Update: 03

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import LISCADPage from './pages/LISCADPage'
 
 const App = (): React.JSX.Element => {
   return (
     <>
       {/* <AutodeskPage /> */}
-      <LISCADPage />
+      {/* <LISCADPage /> */}
     </>
   )
 }
